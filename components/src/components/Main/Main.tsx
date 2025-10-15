d390337ac2983f4da16d8dbb16fdca7ebcb9f906
Oumaima <oumaima.assarti@student.vinci.be>
2025-10-15 10:15:08 +0200

    ex:components

---
+++ components/src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+  return (
+    <main>
+      <p>Ceci est le contenu principal de la page.</p>
+    </main>
+  );
+};
+
+export default Main;
